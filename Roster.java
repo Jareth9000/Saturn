bfc6c06d760c2267fa5b24ee91d04b32a87f604a
AlbertL52 <albertl52@nycstudents.net>
2026-04-28 10:55:40 -0400

    Roster class

---
+++ Roster.java
@@ -0,0 +1,16 @@
+public class Roster {
+    private int OfferingID;
+    private int StudentID;
+
+    private Roster(int OfferingID, int StudentID){
+        this.OfferingID = OfferingID;
+        this.StudentID = StudentID;
+    }
+
+    @Override
+    public String toString(){
+        return "INSERT INTO Rosters (OfferingID, StudentID) VALUES (" + OfferingID + ", " + StudentID + ");";
+    }
+
+
+}
