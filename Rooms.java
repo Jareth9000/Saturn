f20f8661da1137c5fd864b8013f3e792146236c3
AlbertL52 <albertl52@nycstudents.net>
2026-04-28 09:55:44 -0400

    Add files via upload

--- Rooms.java
+++ Rooms.java
@@ -1,11 +1,15 @@
 public class Rooms {
     private String name;
     private int ID;
-    private static int currID;
+    private static int currID = 1;
 
     public Rooms(String name) {
         this.name = name;
         ID = currID++;
+    }
+
+    public int getID() {
+        return ID;
     }
 
     @Override
