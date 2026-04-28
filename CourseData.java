f6b929b84ff664a1dce2183da884b120dac7749b
Jareth9000 <jarethl@nycstudents.net>
2026-04-28 10:49:38 -0400

    Add files via upload

---
+++ CourseData.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CourseData {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src//data"); // reads file with all course data from data doc
+        Scanner scan = new Scanner(file);
+        ArrayList<Course> courses = new ArrayList<>();
+        int type = 0;
+        while (scan.hasNext()) {
+            String str = scan.nextLine();
+            if (str.equals("AP Courses")) {
+                type = 3;
+            } else if (str.equals("Regents Courses")) {
+                type = 2;
+            } else if (str.equals("Elective / Non-AP / Non-Regents Courses")) {
+                type = 1;
+            }
+            Course course = new Course(str,type);
+            courses.add(course);
+        }
+        for (Course course: courses) {
+            System.out.println(course.toString());
+        }
+    }
+}
