5a9e31a830c29f2271d0b4d748e4eabadec13879
Muntasirc3 <muntasirc3@nycstudents.net>
2026-04-28 11:00:22 -0400

    AAAAAAAAAAAHhhhhhh

---
+++ Offerings.java
@@ -0,0 +1,25 @@
+public class Offerings {
+    private static int currID = 1;
+    private int ID;
+    private int period;
+    private int courseID;
+    private int teacherID;
+    private int roomID;
+
+    public Offerings(int period, int courseID, int teacherID, int roomID) {
+        this.period = period;
+        this.courseID = courseID;
+        this.teacherID = teacherID;
+        this.roomID = roomID;
+        ID = currID++;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "INSERT INTO Offerings (CourseID, TeacherID, RoomID, Period) VALUES ('" + courseID + "', '" + teacherID + "', '" + roomID + "', '" + period + "');";
+    }
+}
