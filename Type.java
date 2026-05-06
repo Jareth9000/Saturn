9d0e27bdc51e8386a00f721a178236b73041ba6c
Jareth9000 <jarethl@nycstudents.net>
2026-05-06 10:26:10 -0400

    added getid

--- Type.java
+++ Type.java
@@ -1,14 +1,19 @@
 public class Type {
     private String name;
+    private int ID;
     private static int TypeID = 0;
 
     public Type (String name) {
         this.name = name;
-        TypeID++;
+        ID = TypeID++;
+    }
+
+    public int getID() {
+        return ID;
     }
 
     @Override
     public String toString() {
-        return "INSERT INTO Types (TypeName) VALUES ('" + name + "'");";
+        return "INSERT INTO Types (TypeName, TypeID) VALUES ('" + name + "'," + ID + ");";
     }
 }
