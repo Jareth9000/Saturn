336617a43965d186aac058ba8ae3c2940517c4c5
Muntasirc3 <muntasirc3@nycstudents.net>
2026-05-06 10:30:26 -0400

    Add files via upload

--- TypeData.java
+++ TypeData.java
@@ -2,13 +2,14 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class TypeData {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static ArrayList<Type> makeTypes() throws FileNotFoundException {
         ArrayList<Type> types = new ArrayList<>();
         types.add(new Type("Elective"));
         types.add(new Type("Regents course"));
         types.add(new Type("AP course"));
         for (Type type:types) {
-            System.out.println(type.toString);
+            System.out.println(type.toString());
         }
+        return types;
     }
 }
