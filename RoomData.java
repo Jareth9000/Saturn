fef486a7b91ad2a1467f6d9f65af0fe2df546b5c
AlbertL52 <albertl52@nycstudents.net>
2026-05-06 09:54:45 -0400

    Add files via upload

--- RoomData.java
+++ RoomData.java
@@ -1,22 +1,19 @@
 import java.util.ArrayList;
-import java.io.FileNotFoundException;
 
 public class RoomData {
-    public static ArrayList<Rooms> makeRooms() throws FileNotFoundException {
+    public static ArrayList<Rooms> makeRooms() {
         ArrayList<Rooms> Roomsdata = new ArrayList<>();
         String add = "";
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 4; j++){
                 for(int k = 1; k < 21; k++){
                     add = "";
-                    //first
                     if(i == 0){
                         add += "B";
                     }
                     else{
                         add += i;
                     }
-                    //second
                     if(j == 0){
                         add += "N";
                     }
@@ -29,7 +26,6 @@ public class RoomData {
                     else{
                         add += "W";
                     }
-                    //third
                     add += k;
                     Roomsdata.add(new Rooms(add));
                 }
