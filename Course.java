132b982a1722da3ae7a27ab0b0f9e622b492abf9
Jareth9000 <jarethl@nycstudents.net>
2026-05-06 10:21:49 -0400

    Add files via upload

--- Course.java
+++ Course.java
@@ -1,17 +1,23 @@
 public class Course {
     private String name;
     private int TypeID;
-    private static int CourseID = 0;
+    private int ID;
+    private static int CourseID = 1;
 
 
-    public Course (String name) {
+    public Course (String name, int type) {
         this.name = name;
-        CourseID++;
+        TypeID = type;
+        ID = CourseID++;
+    }
+
+    public int getID() {
+        return ID;
     }
 
     @Override
     public String toString() {
-        return "INSERT INTO Courses (CourseName,TypeID) VALUES ('" + name + "'," + TypeID + ");";
+        return "INSERT INTO Courses (CourseName,TypeID) VALUES ('" + name + "', " + TypeID + ");";
     }
 
 
