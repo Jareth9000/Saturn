2f29f16744f0222b0b7e91b1a52ee1f39f435f1b
AlbertL52 <albertl52@nycstudents.net>
2026-05-12 10:23:15 -0400

    Add files via upload

--- Offerings.java
+++ Offerings.java
@@ -1,10 +1,10 @@
 public class Offerings {
     private static int currID = 1;
-    private int ID;
-    private int period;
-    private int courseID;
-    private int teacherID;
-    private int roomID;
+    private final int ID;
+    private final int period;
+    private final int courseID;
+    private final int teacherID;
+    private final int roomID;
 
     public Offerings(int period, int courseID, int teacherID, int roomID) {
         this.period = period;
