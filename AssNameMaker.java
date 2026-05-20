5701a01512592ef6344968e9c25ee0c2d74e62ac
Muntasirc3 <muntasirc3@nycstudents.net>
2026-05-20 10:03:06 -0400

    Add files via upload

--- AssNameMaker.java
+++ AssNameMaker.java
@@ -3,39 +3,17 @@ import java.util.ArrayList;
 public class AssNameMaker {
     public static ArrayList<AssignmentNames> makeAssignmentNames() {
 
+
         ArrayList<AssignmentNames> assNamesData = new ArrayList<>();
-        AssignmentNames C1 = new AssignmentNames("Class Work 1");
-        AssignmentNames H1 = new AssignmentNames("Homework 1");
-        AssignmentNames C2 = new AssignmentNames("Class Work 2");
-        AssignmentNames H2 = new AssignmentNames("Homework 2");
-        AssignmentNames T1 = new AssignmentNames("Test 1");
-        AssignmentNames C3 = new AssignmentNames("Class Work 3");
-        AssignmentNames H3 = new AssignmentNames("Homework 3");
-        AssignmentNames C4 = new AssignmentNames("Class Work 4");
-        AssignmentNames H4 = new AssignmentNames("Homework 4");
-        AssignmentNames T2 = new AssignmentNames("Test 2");
-        AssignmentNames C5 = new AssignmentNames("Class Work 5");
-        AssignmentNames H5 = new AssignmentNames("Homework 5");
-        AssignmentNames C6 = new AssignmentNames("Class Work 6");
-        AssignmentNames H6 = new AssignmentNames("Homework 6");
-        AssignmentNames T3 = new AssignmentNames("Test 3");
+        for (int i = 1; i <= 6; i++) {
 
-        assNamesData.add(C1);
-        assNamesData.add(H1);
-        assNamesData.add(C2);
-        assNamesData.add(H2);
-        assNamesData.add(T1);
-        assNamesData.add(C3);
-        assNamesData.add(H3);
-        assNamesData.add(C4);
-        assNamesData.add(H4);
-        assNamesData.add(T2);
-        assNamesData.add(C5);
-        assNamesData.add(H5);
-        assNamesData.add(C6);
-        assNamesData.add(H6);
-        assNamesData.add(T3);
-
+            assNamesData.add(new AssignmentNames("Classwork " + i));
+            assNamesData.add(new AssignmentNames("Homework " + i));
+            if (i % 2 == 0) {
+                assNamesData.add(new AssignmentNames("Test " + i / 2));
+            }
+        }
+        System.out.println();
         return assNamesData;
     }
 
--- SaturnDataMaker.java
+++ SaturnDataMaker.java
@@ -5769,39 +5769,17 @@ public class SaturnDataMaker {
 
     public static ArrayList<AssignmentNames> makeAssignmentNames() {
 
+
         ArrayList<AssignmentNames> assNamesData = new ArrayList<>();
-        AssignmentNames C1 = new AssignmentNames("Class Work 1");
-        AssignmentNames H1 = new AssignmentNames("Homework 1");
-        AssignmentNames C2 = new AssignmentNames("Class Work 2");
-        AssignmentNames H2 = new AssignmentNames("Homework 2");
-        AssignmentNames T1 = new AssignmentNames("Test 1");
-        AssignmentNames C3 = new AssignmentNames("Class Work 3");
-        AssignmentNames H3 = new AssignmentNames("Homework 3");
-        AssignmentNames C4 = new AssignmentNames("Class Work 4");
-        AssignmentNames H4 = new AssignmentNames("Homework 4");
-        AssignmentNames T2 = new AssignmentNames("Test 2");
-        AssignmentNames C5 = new AssignmentNames("Class Work 5");
-        AssignmentNames H5 = new AssignmentNames("Homework 5");
-        AssignmentNames C6 = new AssignmentNames("Class Work 6");
-        AssignmentNames H6 = new AssignmentNames("Homework 6");
-        AssignmentNames T3 = new AssignmentNames("Test 3");
+        for (int i = 1; i <= 6; i++) {
 
-        assNamesData.add(C1);
-        assNamesData.add(H1);
-        assNamesData.add(C2);
-        assNamesData.add(H2);
-        assNamesData.add(T1);
-        assNamesData.add(C3);
-        assNamesData.add(H3);
-        assNamesData.add(C4);
-        assNamesData.add(H4);
-        assNamesData.add(T2);
-        assNamesData.add(C5);
-        assNamesData.add(H5);
-        assNamesData.add(C6);
-        assNamesData.add(H6);
-        assNamesData.add(T3);
-
+            assNamesData.add(new AssignmentNames("Classwork " + i));
+            assNamesData.add(new AssignmentNames("Homework " + i));
+            if (i % 2 == 0) {
+                assNamesData.add(new AssignmentNames("Test " + i / 2));
+            }
+        }
+        System.out.println();
         return assNamesData;
     }
 
@@ -5849,19 +5827,5 @@ public class SaturnDataMaker {
             }
         }
         return offers;
-    }
-
-    public static ArrayList<Assignment> makeAssignments(ArrayList<Roster> rosters) {
-        ArrayList<Assignment> assignments = new ArrayList<>();
-        ArrayList<AssignmentNames> assignmentNames = AssNameMaker.makeAssignmentsNames();
-        for (Roster roster : rosters) {
-            for (int i = 0; i < 15; i++) {
-                int grade = (int) (Math.random() * 26) + 75;
-                boolean scale = assignmentNames.get(i).getName().contains("Test");
-                Assignment assignment = new Assignment(assignmentNames.get(i).getID(),roster.getStudentID(),roster.getOfferingID(),scale,grade);
-                assignments.add(assignment);
-            }
-        }
-        return assignments;
     }
 }
