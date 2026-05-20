89f66bbc6803a6bba640834e21a903974b5a6e16
AlbertL52 <albertl52@nycstudents.net>
2026-05-20 10:07:24 -0400

    Updated using file

--- DepartmentData.java
+++ DepartmentData.java
@@ -1,19 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class DepartmentData {
-    public static ArrayList<Department> departmentData() {
-        ArrayList<Department> departments = new ArrayList<>();
-        departments.add(new Department("Biological Sciences"));
-        departments.add(new Department("Chemistry"));
-        departments.add(new Department("CTE, Computer Science & Engineering\n"));
-        departments.add(new Department("English"));
-        departments.add(new Department("Health & PE"));
-        departments.add(new Department("Mathematics"));
-        departments.add(new Department("Physics"));
-        departments.add(new Department("Social Studies"));
-        departments.add(new Department("Special Education"));
-        departments.add(new Department("Visual & Performing Arts"));
-        departments.add(new Department("World Languages & ENL"));
-        return departments;
+public class DepartmentData{
+    public static ArrayList<Department> DepartmentData() throws FileNotFoundException {
+        ArrayList<Department> departmentData = new ArrayList<>();
+        File file = new File("src//DepartmentFile");
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            departmentData.add(new Department(scanner.nextLine()));
+        }
+        return departmentData;
     }
 }
