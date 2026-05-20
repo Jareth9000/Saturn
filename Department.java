a3bf2ed640d4401af4f40cc37b0d89456e446a93
AlbertL52 <albertl52@nycstudents.net>
2026-05-20 10:15:22 -0400

    updated ID issue

--- Department.java
+++ Department.java
@@ -1,6 +1,6 @@
 public class Department {
     private final String name;
-    private static int currID;
+    private static int currID = 1;
     private final int departmentID;
 
     public Department(String name) {
